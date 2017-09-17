c24244c7232e1c5653ffead096fce6bfc0c921c0
Kuzmics Tamás <tanky.hu@gmail.com>
2017-09-18 00:16:20 +0200

    Update tomitankChess.js

--- tomitankChess.js
+++ tomitankChess.js
@@ -1,5 +1,5 @@
 /*
-* TanKy Chess v.1.4.0 by tomitank
+* tomitankChess v.1.4.0 by tomitank
 * Date:2017.09.18.
 * Contact:tomitank@freemail.hu , tanky.hu@gmail.com
 * https://github.com/tomitank/tomitankChess
