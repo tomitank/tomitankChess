e7b75691c23fa917541de1196c33f108585d1f47
Kuzmics Tamás <tanky.hu@gmail.com>
2026-04-01 14:09:01 +0200

    build update

--- build.bat
+++ build.bat
@@ -1,15 +1,24 @@
 @echo off
+
+REM 0. Get node version
+for /f "delims=" %%v in ('node -v') do set NODE_VERSION=%%v
+
+REM remove 'v' from version string
+set NODE_VERSION=%NODE_VERSION:v=%
+echo Node version: %NODE_VERSION%
+set EXE_NAME=tomitankChess_60_x64_node%NODE_VERSION%.exe
+
 REM 1. SEA blob build
 node --experimental-sea-config sea-config.json
 
 REM 2. Copy executable
-copy "%ProgramFiles%\nodejs\node.exe" tomitankChess_60_x64_node24.exe
+copy "%ProgramFiles%\nodejs\node.exe" %EXE_NAME%
 
 REM 3. Remove signature
-REM signtool remove /s tomitankChess_60_x64_node24.exe
+REM signtool remove /s %EXE_NAME%
 
 REM 4. Inject blob
-npx postject tomitankChess_60_x64_node24.exe NODE_SEA_BLOB tomitankChess.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+npx postject %EXE_NAME% NODE_SEA_BLOB tomitankChess.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 
 REM 5. Sign the executable
 
