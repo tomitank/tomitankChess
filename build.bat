80db2a015bc80537c8e9af7e91de8d90eedb673e
Kuzmics Tamás <tanky.hu@gmail.com>
2026-04-01 13:45:43 +0200

    rename

--- build.bat
+++ build.bat
@@ -3,13 +3,13 @@ REM 1. SEA blob build
 node --experimental-sea-config sea-config.json
 
 REM 2. Copy executable
-copy "%ProgramFiles%\nodejs\node.exe" tomitankChess.exe
+copy "%ProgramFiles%\nodejs\node.exe" tomitankChess_60_x64_node24.exe
 
 REM 3. Remove signature
-REM signtool remove /s tomitankChess.exe
+REM signtool remove /s tomitankChess_60_x64_node24.exe
 
 REM 4. Inject blob
-npx postject tomitankChess.exe NODE_SEA_BLOB tomitankChess.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+npx postject tomitankChess_60_x64_node24.exe NODE_SEA_BLOB tomitankChess.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 
 REM 5. Sign the executable
 
